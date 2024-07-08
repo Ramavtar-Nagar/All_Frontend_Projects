8d2c6a1e61de6d4129942ea55332182b6b0ea129
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-07-08 16:20:05 +0530

    Project setup and fetchiong API

---
+++ 05 Crypto Tracker/js/app.js
@@ -0,0 +1,22 @@
+const options = {
+    method: "GET",
+    headers: {
+        accept: "application/json",
+        'x-rapidapi-key': '6fbb01cc58msh39421f2fd714f1fp188e24jsne12c639fae51',
+    }
+}
+
+let coins = [];
+
+const fetchCoins = async () => {
+    try {
+        const response = await fetch("https://coingecko.p.rapidapi.com/coins/markets?page=1&vs_currency=usd&per_page=100&order=market_cap_desc", options);
+        coins = await response.json();
+        console.log(coins);
+        console.log(response)
+    } catch (error) {
+        console.error("Error while fetching coins", error);
+    }
+}
+
+window.onload = fetchCoins();

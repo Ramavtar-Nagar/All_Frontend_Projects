873d548dc45e767e1d89c7e4c491bf595b84d669
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-07-09 00:52:31 +0530

    Displaying data on app

--- 05 Crypto Tracker/js/app.js
+++ 05 Crypto Tracker/js/app.js
@@ -8,16 +8,48 @@ const options = {
 
 let coins = [];
 
+// Step 1 => Fetching the data from the API
 const fetchCoins = async () => {
     try {
         const response = await fetch("https://coingecko.p.rapidapi.com/coins/markets?page=1&vs_currency=usd&per_page=100&order=market_cap_desc", options);
-        coins = await response.json();
-        return coins;
+        const coinsData = await response.json();
+        return coinsData;
         //console.log(coins);
         //console.log(response)
     } catch (error) {
         console.error("Error while fetching coins", error);
     }
+};
+
+const handleFavClick = (coinId) => {
+
 }
 
-window.onload = fetchCoins();
+// Step 2 => display the data on the page
+
+const displayCoins = (coins) => {
+    const tableBody = document.getElementById("crypto-table-body");
+    tableBody.innerHTML = "";
+    coins.forEach((coin, index) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${index}</td>
+            <td><img src="${coin.image}" alt="${coin.name}" width="24" height="24"/></td>
+            <td>${coin.name}</td>
+            <td>$ ${coin.current_price}</td>
+            <td>${coin.total_volume}</td>
+            <td>$ ${coin.market_cap}</td>
+            <td><i class="fa-solid fa-star favourite-icon" data-id="${coin.id}"></i></td>
+        `;
+        row.getElementsByClassName("favourite-icon").addEventListener('click', (event) => {
+            event.stopPropogation();
+            handleFavClick(coin.id);
+        })
+        tableBody.appendChild(row);
+    })
+}
+document.addEventListener("DOMContentLoaded", async () => {
+    coins = await fetchCoins();
+    console.log(coins);
+    displayCoins(coins)
+})

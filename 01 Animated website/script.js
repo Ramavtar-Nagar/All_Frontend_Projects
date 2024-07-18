e0863ffe9db730f5767cee61c5e8437ec795f17d
Ramavtar-Nagar <121015446+Ramavtar-Nagar@users.noreply.github.com>
2024-07-18 23:56:43 +0530

    page 4 js added, issue fixing needed

--- 01 Animated website/script.js
+++ 01 Animated website/script.js
@@ -205,3 +205,26 @@ end: `250% top`,
 });
 }
 canvas()
+
+
+
+// Page 4 Javascript
+var clutter = "";
+
+document.querySelector("#page4>h1").textContent.split("").forEach(function(dets){
+  clutter += `<span> ${dets}</span>`
+  document.querySelector("#page4>h1").innerHTML = clutter;
+})
+
+gsap.to("#page4>h1>span", {
+  scrollTrigger:{
+    trigger:`#page4>h1>span`,
+    start: `top bottom`,
+    end: `bottom top`,
+    scroller:`#main`,
+    scrub:.5,
+  },
+  stagger:.2,
+  color:`#fff`
+})
+
